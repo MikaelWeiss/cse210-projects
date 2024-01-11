a323120ef9534ba4a7c284e33bcd16f358432367
Mikael Weiss <campingmikael@icloud.com>
2024-01-11 13:15:19 -0700

    C# Prep 1

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,11 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.Write("What is your first name? ");
+        string name = Console.ReadLine();
+        Console.Write("What is your last name? ");
+        string lastName = Console.ReadLine();
+        Console.WriteLine("");
+        Console.WriteLine($"Your name is {lastName}, {name} {lastName}");
     }
 }
