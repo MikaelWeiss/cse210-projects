7035341ea712c3ba8fbc2e572a17df540d21b78a
Mikael Weiss <campingmikael@icloud.com>
2024-01-12 14:23:33 -0700

    Completed prep2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,72 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("Please write a grade percentage: ");
+        string input = Console.ReadLine();
+        int grade = int.Parse(input);
+        string letterGrade = "";
+
+        if (grade >= 90)
+        {
+            letterGrade = "A";
+            if (grade <= 92)
+            {
+                letterGrade += "-";
+            }
+        }
+        else if (grade >= 80)
+        {
+            letterGrade = "B";
+
+            if (grade >= 87)
+            {
+                letterGrade += "+";
+            }
+            else if (grade <= 82)
+            {
+                letterGrade += "-";
+            }
+        }
+        else if (grade >= 70)
+        {
+            letterGrade = "C";
+
+            if (grade >= 77)
+            {
+                letterGrade += "+";
+            }
+            else if (grade <= 72)
+            {
+                letterGrade += "-";
+            }
+        }
+        else if (grade >= 60)
+        {
+            letterGrade = "D";
+
+            if (grade >= 67)
+            {
+                letterGrade += "+";
+            }
+            else if (grade <= 62)
+            {
+                letterGrade += "-";
+            }
+        }
+        else
+        {
+            letterGrade = "F";
+        }
+
+        Console.WriteLine($"Your letter grade is {letterGrade}");
+
+        if (grade >= 70)
+        {
+            Console.WriteLine("Congrats! You passed!");
+        }
+        else
+        {
+            Console.WriteLine("So sorry. You failed! Keep trying! You'll get it!");
+        }
     }
 }
