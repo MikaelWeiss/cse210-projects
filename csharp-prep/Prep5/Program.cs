8e2020e7cabff7bea409827a67b47e3dde0acbe2
Mikael Weiss <campingmikael@icloud.com>
2024-01-17 09:51:23 -0700

    prep 5

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -1,9 +1,23 @@
 using System;
+using System.Reflection;
+using System.Reflection.Metadata.Ecma335;
 
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep5 World!");
+
+        int addTwo(int number)
+        {
+            return number + 2;
+        }
+
+        int more = addTwo(10);
+
+        void printName(string name)
+        {
+            Console.WriteLine($"Hello {name}");
+        }
     }
 }
