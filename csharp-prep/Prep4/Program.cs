aa8747c6651aad4428e9ed5d6e4f3e4ad586505d
Mikael Weiss <campingmikael@icloud.com>
2024-01-17 09:38:18 -0700

    Prep 4

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -5,5 +5,11 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep4 World!");
+        var numbers = new List<int> {1, 2};
+        
+        foreach(var number in numbers)
+        {
+            Console.WriteLine($"number is {number}");
+        }
     }
 }
