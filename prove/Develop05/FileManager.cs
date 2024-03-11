7d5f157a58b7dc600c7bffbb24b3c5035c56ffdd
Mikael Weiss <campingmikael@icloud.com>
2024-03-11 13:31:59 -0600

    Formatting

--- prove/Develop05/FileManager.cs
+++ prove/Develop05/FileManager.cs
@@ -1,11 +1,14 @@
-public class FileManager {
-    public static void WriteToFile(string stringValue) {
+public class FileManager
+{
+    public static void WriteToFile(string stringValue)
+    {
         Console.Write("What is the filename for the goals file? ");
         string path = Console.ReadLine();
         File.WriteAllLines(path, [stringValue]);
     }
 
-    public static string ReadFromFile() {
+    public static string ReadFromFile()
+    {
         Console.Write("What is the filename for the goals file? ");
         string path = Console.ReadLine();
         return File.ReadAllText(path);
